6b401584a17170d15364e1b2240da6566b66917d
goddd92 <goddd92@gmail.com>
2015-09-22 12:46:03 +0300

    Idi nahuy

--- StringCalculator/Program.cs
+++ StringCalculator/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace StringCalculator
 {
@@ -10,6 +11,38 @@ namespace StringCalculator
     {
         static void Main(string[] args)
         {
+        }
+    }
+
+    public class StringCalculator
+    {
+        public StringCalculator()
+        {
+
+        }
+        public int Add (string numbers)
+        {
+            return 0;
+        }
+    }
+}
+
+namespace StringCalculator
+{
+    [TestFixture]
+    public class StringCalculatorTest
+    {
+        [Test]
+        public void Add()
+        {
+            StringCalculator source = new StringCalculator();
+            source.Add("0,1,2");
+
+            StringCalculator destination = new StringCalculator();
+            destination.Add("0,1");
+
+            Assert.AreEqual(3, source.Add);
+            Assert.AreEqual(1, source.Add);
         }
     }
 }
